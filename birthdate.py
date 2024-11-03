7226be78ba7ffe7685c5a78f47c20417e8208d51
vidhyacodingal <vidhyacodingal@gmail.com>
2024-11-03 19:16:02 +0530

    commit

---
+++ birthdate.py
@@ -0,0 +1,8 @@
+import datetime
+
+birthday1 = input("Enter sam's birthday in dd/mm/yyyy format")
+birthday2 = input("Enter John's birthday in dd/mm/yyyy format")
+birthday3 = input("Enter Alex's birthday in dd/mm/yyyy format")
+print("Sam's birthday is on", birthday1)
+print("John's birthday is on", birthday2)
+print("Alex's birthday is on", birthday3)
